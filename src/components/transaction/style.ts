39ca905f16cc6ef9bdbfdb28249e145d0bb921eb
wanderley <camposwanderley418@gmail.com>
2024-03-20 18:27:16 -0300

    iniciando component de transaçãoes

---
+++ src/components/transaction/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  position: relative;
+  padding: 0.75rem;
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,19 @@ import { Card } from "../components/card"
 import { Input } from "../components/input"
 import { Logo } from "../components/Logo"
 import { Titile } from "../components/title"
-import { Balance, Filters, Header, InputGroup, Main, Section } from "./style"
+import {
+  Aside,
+  Balance,
+  ChartAction,
+  ChartContainer,
+  ChartContent,
+  Filters,
+  Header,
+  InputGroup,
+  Main,
+  SearchTransaction,
+  Section
+} from "./style"
 
 export function Home() {
   return (
@@ -47,7 +59,48 @@ export function Home() {
             <Card title="Saldo" amount={1000000} variant="incomes" />
             <Card title="Saldo" amount={1000000} variant="expenses" />
           </Balance>
+          <ChartContainer>
+            <header>
+              <Titile
+                Titile="Gastos"
+                subtitle="Despesas por categoria no período"
+              />
+            </header>
+            <ChartContent></ChartContent>
+          </ChartContainer>
+          <ChartContainer>
+            <header>
+              <Titile
+                Titile="Evolução Financeira"
+                subtitle="Saldo, Receitas e Gastos no ano"
+              />
+              <ChartAction>
+                <InputMask
+                  component={Input}
+                  mask="aaaa"
+                  replacement={{ a: /\d/ }}
+                  variant="black"
+                  label="Inicio"
+                  placeholder="aaaa"
+                />
+                <ButtonICon />
+              </ChartAction>
+            </header>
+            <ChartContent></ChartContent>
+          </ChartContainer>
         </Section>
+        <Aside>
+          <header>
+            <Titile
+              Titile="Transações"
+              subtitle="Receitas e Gastos no Periodo"
+            />
+            <SearchTransaction>
+              <Input variant="black" placeholder="Procurar Transação..." />
+              <ButtonICon />
+            </SearchTransaction>
+          </header>
+        </Aside>
       </Main>
     </>
   )
