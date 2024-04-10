93e2ecedb52d7c5c2741a9058880bdef91575c6d
wanderley <camposwanderley418@gmail.com>
2024-04-10 17:13:01 -0300

    criando controle de formulario no dialog de categoria

--- src/components/create-category-dialog/index.tsx
+++ src/components/create-category-dialog/index.tsx
@@ -1,21 +1,39 @@
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useCallback, useState } from "react"
+import { useForm } from "react-hook-form"
 
+import { theme } from "../../styles/themes"
+import { createCategorySchema } from "../../validators/schema"
+import { CreateCategoryData } from "../../validators/types"
 import { Button } from "../button"
 import { Dialog } from "../dialog"
 import { Input } from "../input"
 import { Titile } from "../title"
 import { Container } from "./style"
+import { useFetchAPI } from "../../hooks/useFetchAPI"
 
 export function CreateCategoryDialog() {
+  const { createCategory } = useFetchAPI()
   const [open, setOpen] = useState(false)
+  const { register, handleSubmit, formState } = useForm<CreateCategoryData>({
+    defaultValues: {
+      title: "",
+      color: theme.color.primary
+    },
+    resolver: zodResolver(createCategorySchema)
+  })
 
   const handleClose = useCallback(() => {
     setOpen(false)
   }, [])
 
-  const onSubmit = useCallback(() => {
-    handleClose()
-  }, [handleClose])
+  const onSubmit = useCallback(
+    async (data: CreateCategoryData) => {
+      await createCategory(data)
+      handleClose()
+    },
+    [handleClose, createCategory]
+  )
 
   return (
     <Dialog
@@ -28,10 +46,14 @@ export function CreateCategoryDialog() {
           Titile="Nova Categoria"
           subtitle="Crie uma nova categoria para suas transações"
         />
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <Input label="Nome" placeholder="Nome da categoria..." />
-            <Input label="Cor" type="color" />
+            <Input
+              {...register("title")}
+              label="Nome"
+              placeholder="Nome da categoria..."
+            />
+            <Input {...register("color")} label="Cor" type="color" />
           </div>
           <footer>
             <Button onClick={handleClose} variant="outline" type="button">
